d32c5f975bb90b1a55a476ba093810acc25b005f
Nagendra Kumar <153008051+gcfgf@users.noreply.github.com>
2024-02-14 17:26:02 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,59 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-app';
+ 
+
+isEven  = ""
+count:number=0;
+isNotdisable=false;
+isNotdisable1=false
+
+evenodd(){
+if (this.count%2==0) {
+  this.isEven = "Odd"
+
+} else {
+  this.isEven= "Even"
+}
+
+
+}
+
+
+  inc(){
+
+if(this.count>=20)
+window.alert('Allows Below Twenty '),
+this.isNotdisable = true;
+
+
+else{
+  this.evenodd()
+  this.count++
+  this.isNotdisable1 = false
+ 
+}
+
+
+  }
+  dec(){
+if(this.count<=0){
+this.isNotdisable1 = true
+window.alert('Allows Above Zero  ')
+}
+else{
+  this.evenodd()
+this.count--
+this.isNotdisable=false;
+
+
+}
+ 
+  }
+  res(){
+    this.count=0 
+    this.isNotdisable = false 
+    this.isNotdisable1 = false
+    this.isEven = " "
+  }
 }
